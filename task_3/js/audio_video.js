af03e0ca4b55c3027818b9af15a3807d726299cc
Mansi Devrukhkar <mansidevrukhkar2000@gmail.com>
2021-03-24 15:55:45 +0530

    added task 3

---
+++ task_3/js/audio_video.js
@@ -0,0 +1,23 @@
+
+    var vid = document.getElementById("video1"); 
+    
+    function playVid() { 
+      vid.play(); 
+    } 
+    
+    function pauseVid() { 
+      vid.pause(); 
+    } 
+
+    
+        var x = document.getElementById("audio1"); 
+        
+        function playAudio() { 
+          x.play(); 
+        } 
+        
+        function pauseAudio() { 
+          x.pause(); 
+        } 
+
+    
